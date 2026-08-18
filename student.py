70eeaab9dbe88408d9dd0266a0336dfe101dc008
Abdul Hameed <iamabdulhameed941@gmail.com>
2026-08-18 15:34:26 +0500

    refactor: improve score parameter naming

--- student.py
+++ student.py
@@ -2,9 +2,8 @@ class Student:
     def __init__(self, name):
         self.name = name
         self.scores = []
-    
-    def add_score(self, score):
 
-            if score < 0:
-             raise ValueError("Score cannot be negative")
-            self.scores.append(score)
+    def add_score(self, score_value):
+        if score_value < 0:
+            raise ValueError("Score cannot be negative")
+        self.scores.append(score_value)
