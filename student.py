02b585cd44388e52c1f78d96b68d4e66646a786a
Abdul-Hameed19 <iamabdulhameed941@gmail.com>
2026-08-25 14:47:47 +0500

    Implement average method in student class
    Added average method to calculate the average score.

--- student.py
+++ student.py
@@ -7,3 +7,6 @@ class Student:
         if score_value < 0:
             raise ValueError("Score cannot be negative")
         self.scores.append(score_value)
+
+    def average(self):
+        return sum(self.scores) / len(self.scores)
